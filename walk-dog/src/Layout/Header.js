9553c176b976c68d414d6bf005ed0e462a0a09a1
The-KRD <kaerde91@gmail.com>
2021-08-30 20:38:16 +0200

    add header component and first styling

---
+++ walk-dog/src/Layout/Header.js
@@ -0,0 +1,22 @@
+import dogsImage from "../assets/photo-1444212477490-ca407925329e.jpg";
+import styles from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>Take a Dog For A Walk</h1>
+        <nav className={styles.navigation}>
+          <div> Quotes abuot dogs</div>
+          <div>Somenthing else</div>
+          <button type="button">Your walks</button>
+        </nav>
+      </header>
+      <div className={styles["main-image"]}>
+        <img alt="three little dogs" src={dogsImage} />
+      </div>
+    </>
+  );
+};
+
+export default Header;
