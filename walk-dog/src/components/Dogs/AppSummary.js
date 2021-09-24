4c732beb61f7b43031218df0e10b714e62b3c7ff
The-KRD <kaerde91@gmail.com>
2021-09-24 16:57:17 +0200

    change app summary

--- walk-dog/src/components/Dogs/AppSummary.js
+++ walk-dog/src/components/Dogs/AppSummary.js
@@ -5,10 +5,10 @@ const AppSummary = () => {
     <section className={styles.summary}>
       <h2>An app where you can arrange to walk a dog.</h2>
       <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a esetting, remaining
-        essentially unchanged.
+      An React app where you can arrange to walk a dog.</p>
+      <p>
+
+In app you can filter dogs list by name, description and weight. You can choose date and lenght of the walk and add it to Your Walks cart.
       </p>
     </section>
   );
